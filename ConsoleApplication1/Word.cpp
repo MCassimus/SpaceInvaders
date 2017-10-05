b1988f44e040d4e093d08cb760d1d483d3163ae2
18dcb01 <danielbarnett5000000@gmail.com>
2017-10-05 14:12:32 -0500

    text temp down, but generally working

--- ConsoleApplication1/Word.cpp
+++ ConsoleApplication1/Word.cpp
@@ -1,20 +1,26 @@
 #include "stdafx.h"
 #include "Word.h"
 #include <iostream>
+#include <string>
 
 
 Word::Word(sf::RenderWindow *renderWindow, std::string str) : GameObject(renderWindow)
 {
 	text.setString(str);
-
-	sf::Font font;
-	if (!font.loadFromFile("Anisha Free.otf"))
+	if (!font.loadFromFile("ArcadeFont.ttf"))
 	{
 		std::cout << "unable to load font" << std::endl;
 		system("pause");
 	}
 
 	text.setFont(font);
+
+	rectangle.setSize(sf::Vector2f(14,12));
+
+	text.setCharacterSize(rectangle.getSize().x);
+
+	sf::Vector2i halfTextSize(text.getLocalBounds().width / 2, text.getLocalBounds().height / 2);
+	text.setOrigin(sf::);
 
 	window = renderWindow;
 }
@@ -24,17 +30,21 @@ Word::~Word()
 {
 }
 
+
+//draw word, have to change view size to make not-blurry
 void Word::render()
 {
-	sf::Text newText = text;
-	//window->draw(newText);
+	sf::View oldView = window->getView();
+	window->setView(sf::View(sf::Rect<float>(0, 0, oldView.getSize().x*2,oldView.getSize().y*2)));
+	window->draw(text);
+	window->setView(oldView);
 }
 
 void Word::update()
 {
 	text.setPosition(rectangle.getPosition());
 	text.setFillColor(rectangle.getFillColor());
-	text.setCharacterSize(12);
+	text.setCharacterSize(rectangle.getSize().x);
 }
 
 void Word::setTexture(std::string str)
