cc8bdb99c06201b703892670895ecd0ffabc599b
18dcb01 <danielbarnett5000000@gmail.com>
2017-10-25 13:40:53 -0500

    stuff

--- ConsoleApplication1/spaceInvaders.cpp
+++ ConsoleApplication1/spaceInvaders.cpp
@@ -12,12 +12,17 @@
 int main()
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	//sf::RenderWindow window(sf::VideoMode::getFullscreenModes().at(0), "Space Invaders", sf::Style::Fullscreen);
 	sf::RenderWindow window(sf::VideoMode(620, 496), "Space Invaders");
 =======
 	sf::RenderWindow window(sf::VideoMode::getFullscreenModes().at(0), "Space Invaders", sf::Style::Fullscreen);
 	//sf::RenderWindow window(sf::VideoMode(620, 496, 24), "Space Invaders");
 >>>>>>> e2fdfadd3d4f713701886360fecc7fa60cc33e6f
+=======
+	//sf::RenderWindow window(sf::VideoMode::getFullscreenModes().at(0), "Space Invaders", sf::Style::Fullscreen);
+	sf::RenderWindow window(sf::VideoMode(620, 496, 24), "Space Invaders");
+>>>>>>> 7083b7d083e06c4fa596d8315f2c538ecb15d896
 	window.setFramerateLimit(60);
 	window.setKeyRepeatEnabled(false);
 	window.setMouseCursorVisible(false);
@@ -41,11 +46,10 @@ int main()
 				window.close();
 			else if (event.type == sf::Event::Resized)
 			{
-				double ratio = event.size.width / view.getSize().x;
-				view.setCenter(view.getSize().x / 2, view.getSize().y / 2);
-				sf::VideoMode vidModeTemp(view.getSize().x, view.getSize().y, 24);
+				double ratio = view.getSize().x / view.getSize().y;
+				sf::VideoMode vidModeTemp(event.size.width, event.size.height / ratio , 24);
 				window.close();
-				window.create(vidModeTemp, "Space Invaders", sf::Style::Fullscreen);
+				window.create(vidModeTemp, "Space Invaders");
 				window.setView(view);
 			}
 			else if (event.type == sf::Event::KeyPressed)
